8603fdee410f9a2456f5d538aa6afdb7b9c0df39
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-07-26 14:30:57 +0200

    cleanup

--- index.js
+++ index.js
@@ -10,59 +10,54 @@ class HybridChunkStore {
   constructor (chunkLength, opts = {}) {
     this.chunkLength = Number(chunkLength)
     if (!this.chunkLength) throw new Error('First argument must be a chunk length')
-    this.length = opts.length
-    this.opts = opts
 
     this.fallbackStore = null
     this.chunkCount = null
     this.stores = []
     this.chunks = []
 
+    // this is kinda stupid, first it makes the fallback store, then the main store
+    // creates a store limited by targetLength, then uses memory as fallback/overflow
+    const _mapStore = (TargetStore, targetLenght) => {
+      const newOpts = opts
+      if (targetLenght && targetLenght < opts.length) {
+        this.chunkCount = Math.floor(targetLenght / this.chunkLength)
+        const newLenght = this.chunkCount * this.chunkLength
+        newOpts.length = opts.length - newLenght
+        // ideally this should be blob store, some1 make one pls
+        this.fallbackStore = new MemoryChunkStore(this.chunkLength, newOpts)
+        this.stores.push(this.fallbackStore)
+        newOpts.length = newLenght
+      }
+      const store = new CacheChunkStore(new TargetStore(this.chunkLength, newOpts), { max: opts.max })
+      this.stores.push(store)
+      if (this.chunkCount) {
+        this.chunks[this.chunkCount - 1] = store
+        this.chunks.fill(store)
+      } else {
+        this.fallbackStore = store
+      }
+    }
+
     this.registration = navigator.storage.estimate().then(estimate => {
       // use less than available
       const remaining = estimate.quota - estimate.usage - Math.max(Number(opts.reserved) || 0, 16777216)
       if ('getDirectory' in navigator.storage) {
         // lets hope the user isn't stupid enough to specify a directory with barely any storage, forgive me tech support people
-        this._mapStore(FSAccessChunkStore, !(opts.rootDir) && remaining)
+        _mapStore(FSAccessChunkStore, !(opts.rootDir) && remaining)
       } else {
         // WAH. https://i.kym-cdn.com/entries/icons/original/000/027/528/519.png
-        this._mapStore(IDBChunkStore, !(isChrome && estimate.quota === 2147483648) && remaining)
+        _mapStore(IDBChunkStore, !(isChrome && estimate.quota === 2147483648) && remaining)
       }
     })
-  }
-
-  // this is kinda stupid, first it makes the fallback store, then the main store
-  // creates a store limited by targetLength, then uses memory as fallback/overflow
-  _mapStore (TargetStore, targetLenght) {
-    const newOpts = this.opts
-    console.log(targetLenght < this.length, targetLenght, this.length)
-    if (targetLenght && targetLenght < this.length) {
-      this.chunkCount = Math.floor(targetLenght / this.chunkLength)
-      const newLenght = this.chunkCount * this.chunkLength
-      newOpts.length = this.length - newLenght
-      // ideally this should be blob store, some1 make one pls
-      this.fallbackStore = new MemoryChunkStore(this.chunkLength, newOpts)
-      this.stores.push(this.fallbackStore)
-      newOpts.length = newLenght
-    }
-    const store = new CacheChunkStore(new TargetStore(this.chunkLength, newOpts), { max: this.opts.max })
-    this.stores.push(store)
-    if (this.chunkCount) {
-      this.chunks[this.chunkCount - 1] = store
-      this.chunks.fill(store)
-    } else {
-      this.fallbackStore = store
-    }
   }
 
   get (index, opts, cb) {
     this.registration.then(() => {
       if (!this.chunks[index]) {
         this.fallbackStore.get(index - this.chunkCount, opts, cb)
-        console.log(this.fallbackStore, index)
       } else {
         this.chunks[index].get(index, opts, cb)
-        console.log(this.chunks[index], index)
       }
     })
   }
