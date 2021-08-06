6292d8d74b003b1b73688821fbed6173c4f904f4
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-08-06 22:19:44 +0200

    handle length

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ class HybridChunkStore {
     this.chunkLength = Number(chunkLength)
     if (!this.chunkLength) throw new Error('First argument must be a chunk length')
 
+    this.length = Number(opts.length) || Infinity
+
     this.fallbackStore = null
     this.chunkCount = null
     this.stores = []
@@ -20,10 +22,10 @@ class HybridChunkStore {
     // creates a store limited by targetLength, then uses memory as fallback/overflow
     const _mapStore = (TargetStore, targetLenght) => {
       const newOpts = opts
-      if (targetLenght && targetLenght < opts.length) {
+      if (targetLenght && targetLenght < this.length) {
         this.chunkCount = Math.floor(targetLenght / this.chunkLength)
         const newLenght = this.chunkCount * this.chunkLength
-        newOpts.length = opts.length - newLenght
+        newOpts.length = this.length - newLenght
         // ideally this should be blob store, some1 make one pls
         this.fallbackStore = new MemoryChunkStore(this.chunkLength, newOpts)
         this.stores.push(this.fallbackStore)
