df8e07b3dd632ca0e1cb3f860522a4b90aad4637
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-11-02 13:42:01 +0100

    chore: update FSA

--- index.js
+++ index.js
@@ -18,29 +18,6 @@ export default class HybridChunkStore {
     this.stores = []
     this.chunks = []
 
-    // this is kinda stupid, first it makes the fallback store, then the main store
-    // creates a store limited by targetLength, then uses memory as fallback/overflow
-    // if targetlength is falsy then it will assume infinite storage
-    const _mapStore = (TargetStore, targetLength) => {
-      const newOpts = opts
-      if (targetLength && targetLength < this.length) {
-        this.chunkCount = Math.floor(targetLength / this.chunkLength)
-        const newLength = this.chunkCount * this.chunkLength
-        newOpts.length = this.length - newLength
-        // ideally this should be blob store, some1 make one pls
-        this.fallbackStore = new MemoryChunkStore(this.chunkLength, newOpts)
-        this.stores.push(this.fallbackStore)
-        newOpts.length = newLength
-      }
-      const store = new CacheChunkStore(new TargetStore(this.chunkLength, newOpts), { max: opts.max || 20 })
-      this.stores.push(store)
-      if (this.chunkCount) {
-        this.chunks[this.chunkCount - 1] = store
-        this.chunks.fill(store)
-      } else {
-        this.fallbackStore = store
-      }
-    }
     this.registration = navigator.storage.estimate().then(estimate => {
       // use less than available
       const remaining = estimate.quota - estimate.usage - Math.max(Number(opts.reserved) || 0, 16777216)
@@ -53,15 +30,39 @@ export default class HybridChunkStore {
         if ('getDirectory' in navigator.storage) {
         // lets hope the user isn't stupid enough to specify a directory with barely any storage, forgive me tech support people
         // can't detect avaliable quota in custom folders
-          _mapStore(FSAccessChunkStore, !(opts.rootDir) && remaining)
+          this._mapStore(FSAccessChunkStore, !(opts.rootDir) && remaining, opts)
         } else {
         // WAH. https://i.kym-cdn.com/entries/icons/original/000/027/528/519.png
         // some OS versions and some chromium browsers report quota as 2^31 when it has more than that available
         // this means we can't estimate how much space they have... oh well
-          _mapStore(IDBChunkStore, !(isChrome && estimate.quota === 2147483648) && remaining)
+          this._mapStore(IDBChunkStore, !(isChrome && estimate.quota === 2147483648) && remaining, opts)
         }
       }
     })
+  }
+
+  // this is kinda stupid, first it makes the fallback store, then the main store
+  // creates a store limited by targetLength, then uses memory as fallback/overflow
+  // if targetlength is falsy then it will assume infinite storage
+  _mapStore (TargetStore, targetLength, opts) {
+    const newOpts = opts
+    if (targetLength && targetLength < this.length) {
+      this.chunkCount = Math.floor(targetLength / this.chunkLength)
+      const newLength = this.chunkCount * this.chunkLength
+      newOpts.length = this.length - newLength
+      // ideally this should be blob store, some1 make one pls
+      this.fallbackStore = new MemoryChunkStore(this.chunkLength, newOpts)
+      this.stores.push(this.fallbackStore)
+      newOpts.length = newLength
+    }
+    const store = new CacheChunkStore(new TargetStore(this.chunkLength, newOpts), { max: opts.max || 20 })
+    this.stores.push(store)
+    if (this.chunkCount) {
+      this.chunks[this.chunkCount - 1] = store
+      this.chunks.fill(store)
+    } else {
+      this.fallbackStore = store
+    }
   }
 
   get (index, opts, cb) {
